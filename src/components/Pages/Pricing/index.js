5928787ce4443a590e5acc3cf9557d555bef80af
yamiuchi <50206848+yamiuchi@users.noreply.github.com>
2021-09-18 00:24:53 +0700

    bcavapayment

--- src/components/Pages/Pricing/index.js
+++ src/components/Pages/Pricing/index.js
@@ -1,79 +1,34 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../../App";
 import axios from "axios";
+import EncryptStorage from "encrypt-storage";
 import { generateFormData } from "../../../utils/generateFormData";
 
 import "./pricing.css";
 
-// Sandbox Environment: https://api.sandbox.midtrans.com
-// Production Environment: https://api.midtrans.com
-
-// const paymentData = {
-//   payment_type: "credit_card",
-//   transaction_details: {
-//     order_id: "C17550",
-//     gross_amount: 145000,
-//   },
-//   credit_card: {
-//     token_id: "< your token ID >",
-//   },
-//   item_details: [
-//     {
-//       id: "a1",
-//       price: 145000,
-//       quantity: 2,
-//       name: "Apel",
-//       brand: "Fuji Apple",
-//       category: "Fruit",
-//       merchant_name: "Fruit-store",
-//     },
-//   ],
-//   customer_details: {
-//     first_name: "BUDI",
-//     last_name: "UTOMO",
-//     email: "test@midtrans.com",
-//     phone: "+628123456",
-//     billing_address: {
-//       first_name: "BUDI",
-//       last_name: "UTOMO",
-//       email: "test@midtrans.com",
-//       phone: "081 2233 44-55",
-//       address: "Sudirman",
-//       city: "Jakarta",
-//       postal_code: "12190",
-//       country_code: "IDN",
-//     },
-//     shipping_address: {
-//       first_name: "BUDI",
-//       last_name: "UTOMO",
-//       email: "test@midtrans.com",
-//       phone: "0 8128-75 7-9338",
-//       address: "Sudirman",
-//       city: "Jakarta",
-//       postal_code: "12190",
-//       country_code: "IDN",
-//     },
-//   },
-// };
-
 const Pricing = () => {
   const appContext = useContext(AppContext);
 
-  const handleClickGetPaymentStatus = async (params) => {
+  const encryptStorage = EncryptStorage(
+    `${process.env.REACT_APP_LOCAL_STORAGE_SECRET_KEY}`
+  );
+  const [isBCAVAPayment, setIsBCAVAPayment] = useState(false);
+  const [isCCPayment, setIsCCPayment] = useState(false);
+  const [isOpenChoosePayment, setIsOpenChoosePayment] = useState(false);
+  const [packageID, setPackageID] = useState(null);
+  const [packages, setPackages] = useState([]);
+
+  const doCCPayment = async (params) => {
     appContext.setIsLoading(true);
 
     const formData = generateFormData({
-      premiumLevel: params,
+      tokenID: params,
     });
 
     axios
-      .post(
-        `${process.env.REACT_APP_SITE_API_URL}/getmidtranspaymentstatus`,
-        formData,
-        {
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        }
-      )
+      .post(`${process.env.REACT_APP_SITE_API_URL}/ccpayment`, formData, {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      })
       .then((res) => {
         //success
         appContext.setIsLoading(false);
@@ -92,7 +47,70 @@ const Pricing = () => {
       });
   };
 
-  const handleClickSetPremium = async (params) => {
+  const fetchPackages = async () => {
+    appContext.setIsLoading(true);
+
+    axios
+      .get(`${process.env.REACT_APP_SITE_API_URL}/getpackages`, {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      })
+      .then((res) => {
+        //success
+        appContext.setIsLoading(false);
+        setPackages(res.data.result);
+      })
+      .catch((err) => {
+        //error
+        if (err.response) {
+          console.log("res error", err.response.data);
+        } else if (err.request) {
+          console.log("req error", err.request.data);
+        } else {
+          console.log("Error", err.message);
+        }
+        appContext.setIsLoading(false);
+      });
+  };
+
+  const handleClickBCAVAPayment = () => {
+    setIsBCAVAPayment(true);
+  };
+
+  const handleClickCCPayment = () => {
+    setIsCCPayment(true);
+  };
+
+  const handleClickDoBCAVAPayment = () => {
+    appContext.setIsLoading(true);
+
+    const formData = generateFormData({
+      packageID: packageID,
+      userID: encryptStorage.getItem("user_logged_in").user_id,
+    });
+
+    axios
+      .post(`${process.env.REACT_APP_SITE_API_URL}/bcavapayment`, formData, {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      })
+      .then((res) => {
+        //success
+        appContext.setIsLoading(false);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        //error
+        if (err.response) {
+          console.log("res error", err.response.data);
+        } else if (err.request) {
+          console.log("req error", err.request.data);
+        } else {
+          console.log("Error", err.message);
+        }
+        appContext.setIsLoading(false);
+      });
+  };
+
+  const handleClickDoCCPayment = () => {
     appContext.setIsLoading(true);
 
     const cardData = {
@@ -111,31 +129,8 @@ const Pricing = () => {
         console.log("This is the card token_id:", token_id);
         // Implement sending the token_id to backend to proceed to next step
 
-        const formData = generateFormData({
-          premiumLevel: params,
-          tokenID: token_id,
-        });
-
-        axios
-          .post(`${process.env.REACT_APP_SITE_API_URL}/setpremium`, formData, {
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          })
-          .then((res) => {
-            //success
-            appContext.setIsLoading(false);
-            console.log(res.data);
-          })
-          .catch((err) => {
-            //error
-            if (err.response) {
-              console.log("res error", err.response.data);
-            } else if (err.request) {
-              console.log("req error", err.request.data);
-            } else {
-              console.log("Error", err.message);
-            }
-            appContext.setIsLoading(false);
-          });
+        appContext.setIsLoading(false);
+        doCCPayment(token_id);
       },
       onFailure: function (response) {
         // Fail to get card token_id, implement as you wish here
@@ -150,31 +145,98 @@ const Pricing = () => {
     window.MidtransNew3ds.getCardToken(cardData, options);
   };
 
+  const handleClickSelectPackage = (params) => {
+    setIsOpenChoosePayment(true);
+    setPackageID(params);
+  };
+
   useEffect(() => {
     document.title = "Pricing";
+    fetchPackages();
 
     const midtransScriptUrl =
       "https://api.midtrans.com/v2/assets/js/midtrans-new-3ds.min.js";
-    const myMidtransEnvironment = "sandbox";
-    const myMidtransClientKey = "SB-Mid-client-EDrxxrjiUc7sEj2j";
 
     let scriptTag = document.createElement("script");
     scriptTag.src = midtransScriptUrl;
     scriptTag.id = "midtrans-script";
     scriptTag.type = "text/javascript";
-    scriptTag.setAttribute("data-client-key", myMidtransClientKey);
-    scriptTag.setAttribute("data-environment", myMidtransEnvironment);
-
+    scriptTag.setAttribute(
+      "data-client-key",
+      process.env.REACT_APP_MIDTRANS_CLIENT_KEY
+    );
+    scriptTag.setAttribute("data-environment", "sandbox");
     document.body.appendChild(scriptTag);
+
     return () => {
       document.body.removeChild(scriptTag);
     };
+    // eslint-disable-next-line
   }, []);
 
   return (
     <div className="navbar-margin">
       <div className="pricing">
-        <button onClick={() => handleClickSetPremium(1)}>pay</button>
+        {isOpenChoosePayment && (
+          <div>
+            <div onClick={() => setIsOpenChoosePayment(!isOpenChoosePayment)}>
+              close
+            </div>
+            <div onClick={() => handleClickBCAVAPayment()}>bca va</div>
+            {isBCAVAPayment && (
+              <button
+                onClick={() => {
+                  handleClickDoBCAVAPayment();
+                }}
+              >
+                pay
+              </button>
+            )}
+            <div onClick={() => handleClickCCPayment()}>credit card</div>
+            {isCCPayment && (
+              <div>
+                <input
+                  type="text"
+                  name="card_number"
+                  placeholder="card_number"
+                />
+                <input
+                  type="text"
+                  name="card_exp_month"
+                  placeholder="card_exp_month"
+                />
+                <input
+                  type="text"
+                  name="card_exp_year"
+                  placeholder="card_exp_year"
+                />
+                <input type="text" name="card_cvv" placeholder="card_cvv" />
+                <button
+                  onClick={() => {
+                    handleClickDoCCPayment();
+                  }}
+                >
+                  pay
+                </button>
+              </div>
+            )}
+          </div>
+        )}
+
+        {packages
+          ? packages.map((props) => {
+              const { package_id, package_price, package_name } = props;
+              return (
+                <div className="package" key={package_id}>
+                  <div>{package_name}</div>
+                  <div>{package_price}</div>
+                  <button onClick={() => handleClickSelectPackage(package_id)}>
+                    select
+                  </button>
+                </div>
+              );
+            })
+          : "no package"}
       </div>
     </div>
   );
